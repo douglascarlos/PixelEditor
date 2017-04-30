efbb612abc62f0499cb476e84acf135833703263
douglascarlos <dnevescarlos@gmail.com>
2017-04-30 01:45:55 -0300

    alterando input text para select

--- js/app.js
+++ js/app.js
@@ -10,23 +10,12 @@ $(document).ready(init);
 
 function init(){
     materializeSetup();
-    $('#menu_translate').on('click', handleClickMenuTranslate);
-    $('#menu_increase_decrease').on('click', handleClickMenuIncreaseAndDecrease);
 };
 
 function materializeSetup(){
+    $('select').material_select();
 	$('.materialboxed').materialbox();
     $(".button-collapse").sideNav({
 	    menuWidth: 450,
 	});
-}
-
-function handleClickMenuTranslate(){
-	document.getElementById('translate_size').value = '0';
-	document.getElementById('translate_direction').value = 'H';
-}
-
-function handleClickMenuIncreaseAndDecrease(){
-	document.getElementById('increase_decrease_percentage').value = '0';
-	document.getElementById('increase_decrease_type').value = 'I';
 }
